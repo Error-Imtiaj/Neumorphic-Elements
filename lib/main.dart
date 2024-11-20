da75766e7aebff8882d0d34b69e14e2ae136e8d7
Error-Imtiaj <imtiaj0044@gmail.com>
2024-11-21 00:17:45 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:neumorphism/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
